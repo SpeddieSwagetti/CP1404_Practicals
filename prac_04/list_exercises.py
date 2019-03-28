69978dbef08d0cc7848e4bbd2d41684c0c43dfd8
SpeddieSwagetti <mitchjmarks@outlook.com>
2019-03-29 08:02:51 +1000

    start list_exercises.py complete basic list operations task

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,16 @@
+numbers = []
+
+print("Type 5 numbers")
+for i in range(0, 5, 1):
+    while True:
+        try:
+            user_input = int(input("Number: "))
+            numbers.append(user_input)
+            break
+        except ValueError or NameError:
+            print("Try Again")
+print("The first number is {}".format(numbers[0]))
+print("The last number is {}".format(numbers[4]))
+print("The smallest number is {}".format(min(numbers)))
+print("The largest number is {}".format(max(numbers)))
+print("The average of the numbers is {}".format(sum(numbers)/len(numbers)))
