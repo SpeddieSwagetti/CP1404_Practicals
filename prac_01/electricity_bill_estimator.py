41b32ea8e94a066b753d868626ab23bc6032bf2f
SpeddieSwagetti <mitchjmarks@outlook.com>
2019-04-30 12:36:00 +1000

    modify electricity_bill_estimator.py import KivyDemos directory complete box_layout.kv complete box_layout_demo.py

--- prac_01/electricity_bill_estimator.py
+++ prac_01/electricity_bill_estimator.py
@@ -5,4 +5,4 @@ daily_use = float(input("Enter kWh usage per day:"))
 num_of_days = int(input("Enter number of days in billing period:"))
 
 estimated_bill = price * 0.01 * daily_use * num_of_days
-print("Congradulations you millenial scum! Your bill is:",estimated_bill)
+print("Congradulations you millenials! Your bill is: {}. Maybe stop buying icecream".format(estimated_bill))
