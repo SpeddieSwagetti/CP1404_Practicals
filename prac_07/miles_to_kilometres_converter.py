f9395bbaf17ab5f80d82a408a1f5dee530fa61e7
SpeddieSwagetti <mitchjmarks@outlook.com>
2019-05-06 16:13:18 +1000

    miles_to_kilometres_converter.py: import StringProperty from kivy.app add "kilometres" variable add global constant "MILES_TO_KM" instead of constant in "handle_calculation()" refactor "value" parameter to "text" in "handle_calculation()" refactor result parameter to miles globally remove "self.root.ids.output_label.text" implement "miles_to_kilometres()" implement static method "convert_input_to_float()" for input validation fix unnecessary "int()" conversions
    miles_to_kilometres_converter.kv:
    refactor "id: output_label" to "app.kilometres()"
    remove functionality for "Button: "Convert mi to km""
    implement "app.handle_calculation()" in "TextInput: id: input_number"
    remove "multiline: False"
    fix positional argument string formatting in "app.handle_increment()"
    make "Label:app.kilometres" color yellow

--- prac_07/miles_to_kilometres_converter.py
+++ prac_07/miles_to_kilometres_converter.py
@@ -6,10 +6,15 @@ Started 06/05/2019"""
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.app import StringProperty
+
+MILES_TO_KM = 1.60934
 
 
 class MilesToKilometres(App):
     """kivy App for converting Miles to Kilometres"""
+    kilometres = StringProperty()
+
     def build(self):
         """build the kivy app from the kv file"""
         Window.size = (400, 200)
@@ -17,14 +22,27 @@ class MilesToKilometres(App):
         self.root = Builder.load_file('miles_to_kilometres_converter.kv')
         return self.root
 
-    def handle_calculation(self, value):
-        """ handle calculation (could be button press or other call), output result to label widget """
-        result = value * 1.60934
-        self.root.ids.output_label.text = str(result)
+    def handle_calculation(self, text):
+        """ handle calculation (could be button press or other call), output miles to label widget """
+        print("handle_calculation")
+        miles = self.convert_input_to_float(text)
+        self.miles_to_kilometres(miles)
 
-    def handle_increment(self, value, increment):
-        result = int(value) + int(increment)
-        self.root.ids.input_number.text = str(result)
+    def handle_increment(self, text, increment):
+        miles = self.convert_input_to_float(text) + increment
+        self.root.ids.input_number.text = str(miles)
+
+    def miles_to_kilometres(self, miles):
+        print("final conversion from miles to kilometers")
+        self.kilometres = str(miles * MILES_TO_KM)
+
+    @staticmethod
+    def convert_input_to_float(text):
+        try:
+            value = float(text)
+            return value
+        except ValueError:
+            return 0.0
 
 
 MilesToKilometres().run()
